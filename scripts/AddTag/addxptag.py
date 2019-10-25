55850130d7d8d74ab124746f0d8e1ff77ec8ec76
AnnChi <50226428+AsheAnn@users.noreply.github.com>
2019-10-25 11:59:45 +0800

    Add files via upload

---
+++ scripts/AddTag/addxptag.py
@@ -0,0 +1,20 @@
+# ----------------------------------------------------------------------
+# Note:                                                                                                                                                   
+#     - This is the Python code used in Script Manager.                                                                                                                                                  ##
+# Compatible:                                                                                                                                         
+#     - Win / Mac                                                                                                                                 
+#     - R21                                                                                                                                
+# ----------------------------------------------------------------------
+
+
+import c4d
+
+# Main function
+def main():
+    c4d.CallCommand(100004708, 100004708) # Large Icons
+    c4d.CallCommand(100004788, 50059) # New Xpresso Tag
+
+
+# Execute main()
+if __name__ == "__main__":
+    main()
