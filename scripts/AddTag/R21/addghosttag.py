4961e5690585f9508ec01aa697985b5393ca91a7
AnnChi <50226428+AsheAnn@users.noreply.github.com>
2019-11-22 16:18:50 +0800

    Add files via upload

---
+++ scripts/AddTag/R21/addghosttag.py
@@ -0,0 +1,21 @@
+# ----------------------------------------------------------------------
+# Note:
+#     - This is the Python code used in Script Manager.                                                                                                                                                  ##
+# Compatible:
+#     - Win / Mac
+#     - R21
+# ----------------------------------------------------------------------
+
+
+import c4d
+
+# Main function
+def main():
+    c4d.CallCommand(100004708, 100004708) # Large Icons
+    c4d.CallCommand(180000044, 180000044) # Ghost Body Tag
+
+
+
+# Execute main()
+if __name__=='__main__':
+    main()
